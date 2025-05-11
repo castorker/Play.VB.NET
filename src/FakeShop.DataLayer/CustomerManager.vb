c168cd0a1a53abbcc9859bc54fdb0b00e7de3e26
Antonio Silva <ae.osilva@hotmail.com>
2025-05-11 16:01:41 +0100

    Added support to load customers from a XML file.

---
+++ src/FakeShop.DataLayer/CustomerManager.vb
@@ -0,0 +1,64 @@
+﻿Imports System.Collections.ObjectModel
+Imports System.Configuration
+Imports FakeShop.Common
+Imports FakeShop.EntityLayer
+
+Public Class CustomerManager
+    Inherits DataManagerBase
+
+    Function LoadCustomers() As ObservableCollection(Of Customer)
+        Return LoadCustomers(Nothing)
+    End Function
+
+    Function LoadCustomers(ByVal startingFilePath As String) As ObservableCollection(Of Customer)
+        ' Create a new instance of customers
+        Dim ret = New ObservableCollection(Of Customer)()
+
+        Try
+            ' Attempt to read from XML file
+            Dim doc = MyBase.LoadXElement(
+                ConfigurationManager.AppSettings("CustomersFile"),
+            startingFilePath)
+
+            Dim customers = From cust In doc.<Customer>
+                            Select New Customer With {
+                                .CustomerId = Convert.ToInt32(cust.Element("CustomerId").Value),
+                                .Name = cust.Element("Name").Value,
+                                .Email = cust.Element("Email").Value,
+                                .Address = cust.Element("Address").Value,
+                                .PhoneNumber = cust.Element("PhoneNumber").Value
+                            }
+
+            ret = New ObservableCollection(Of Customer)(customers.ToList())
+
+        Catch ex As Exception
+            System.Diagnostics.Debug.WriteLine(ex.ToString())
+        End Try
+
+        Return ret
+    End Function
+
+    Function LoadCustomer(ByVal customerId As Integer) As Customer
+        Return LoadCustomer(customerId, Nothing)
+    End Function
+
+    Function LoadCustomer(ByVal customerId As Integer, ByVal startingFilePath As String) As Customer
+        ' Create a new instance of a customer
+        Dim ret = New Customer()
+
+        Try
+            Dim list = LoadCustomers(startingFilePath)
+
+            If list IsNot Nothing Then
+                If list.Count > 0 Then
+                    ret = list.FirstOrDefault(Function(c) c.CustomerId = customerId)
+                End If
+            End If
+
+        Catch ex As Exception
+            System.Diagnostics.Debug.WriteLine(ex.ToString())
+        End Try
+
+        Return ret
+    End Function
+End Class
--- src/FakeShop.ViewModelLayer/CustomerViewModel.vb
+++ src/FakeShop.ViewModelLayer/CustomerViewModel.vb
@@ -1,25 +1,51 @@
-﻿Imports FakeShop.Common
+﻿Imports System.Collections.ObjectModel
+Imports FakeShop.Common
+Imports FakeShop.DataLayer
 Imports FakeShop.EntityLayer
 
 Public Class CustomerViewModel
     Inherits ViewModelBase
 
     Sub New()
-        LoadCustomer(1)
+        LoadCustomers()
+        LoadCustomer(6)
     End Sub
+
+    Public Property Customers As ObservableCollection(Of Customer)
 
     Public Property Entity As Customer
 
-    Function LoadCustomer(ByVal customerId As Integer) As Customer
-        ' Hard-code an entity
-        Entity = New Customer With {
-            .CustomerId = 1,
-            .Name = "Alice Johnson",
-            .Email = "alice.johnson@example.com",
-            .Address = "123 Maple Street, Lisbon",
-            .PhoneNumber = "+351 912345678"
-        }
+    Function LoadCustomers(ByVal Optional startingFilePath As String = "") As ObservableCollection(Of Customer)
+        ' Create a new instance of customer manager
+        Dim mgr = New CustomerManager
+
+        Customers = mgr.LoadCustomers(startingFilePath)
+
+        Return Customers
+    End Function
+
+    Function LoadCustomer(ByVal customerId As Integer, ByVal Optional startingFilePath As String = "") As Customer
+        ' Create a new instance of customer manager
+        Dim mgr = New CustomerManager
+
+        Entity = mgr.LoadCustomer(customerId, startingFilePath)
+
+        ' Inform UI that the Entity property changed
+        RaisePropertyChanged("Entity")
 
         Return Entity
     End Function
+
+    'Function LoadCustomer(ByVal customerId As Integer) As Customer
+    '    ' Hard-coded an entity
+    '    Entity = New Customer With {
+    '        .CustomerId = 1,
+    '        .Name = "Alice Johnson",
+    '        .Email = "alice.johnson@example.com",
+    '        .Address = "123 Maple Street, Lisbon",
+    '        .PhoneNumber = "+351 912345678"
+    '    }
+
+    '    Return Entity
+    'End Function
 End Class
